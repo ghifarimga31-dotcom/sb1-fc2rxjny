d320d680100117587e515edd5bae2f9c2e51c1b1
ghifarimga31-dotcom <ghifarimga31@gmail.com>
2025-08-23 14:54:08 +0700

    Embed n8n chat with custom styling

--- src/App.tsx
+++ src/App.tsx
@@ -52,9 +52,6 @@ function App() {
         ) : (
           projectId && <ProjectDetail projectId={projectId} />
         )}
-        
-        <ChatWidget />
-        
       </div>
     </ThemeProvider>
   );
